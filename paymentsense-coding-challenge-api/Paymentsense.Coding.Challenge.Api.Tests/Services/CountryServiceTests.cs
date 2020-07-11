d64e32b1623e1f33ea994571d0e89d1089a2b33e
Alexandre Fernandes <mteknight@gmail.com>
2020-07-12 00:26:31 +0100

    created failing test to get all country names in api

---
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api.Tests/Services/CountryServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace Paymentsense.Coding.Challenge.Api.Tests.Services
+{
+    public class CountryServiceTests
+    {
+        [Fact]
+        public void GetNames_OnInvoke_ReturnsCountryNames()
+        {
+            // Arrange
+            var sut = new CountryService();
+
+            // Act
+            var countries = sut.GetNames();
+
+            // Assert
+            countries.Should().NotBeEmpty();
+        }
+    }
+
+    public class CountryService
+    {
+        public IEnumerable<string> GetNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
