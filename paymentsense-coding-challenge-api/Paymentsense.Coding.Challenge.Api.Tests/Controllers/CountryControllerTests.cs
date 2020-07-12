fe17c4dcb32e2190e20f8b811459398ebc065f5c
Alexandre Fernandes <mteknight@gmail.com>
2020-07-12 22:39:19 +0100

    renamed controller and added failing test to get country names

---
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api.Tests/Controllers/CountryControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using Paymentsense.Coding.Challenge.Api.Controllers;
+using Paymentsense.Coding.Challenge.Api.Models;
+using Paymentsense.Coding.Challenge.Api.Services;
+
+using Xunit;
+
+namespace Paymentsense.Coding.Challenge.Api.Tests.Controllers
+{
+    public class CountryControllerTests
+    {
+        private readonly CountryController _controller;
+
+        public CountryControllerTests()
+        {
+            var mockedService = new Mock<ICountryService>();
+
+            _controller = new CountryController(mockedService.Object);
+        }
+
+        [Fact]
+        public void Get_OnInvoke_ReturnsExpectedMessage()
+        {
+            var result = _controller.Get().Result as OkObjectResult;
+
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
+            result.Value.Should().Be("Paymentsense Coding Challenge!");
+        }
+
+        [Fact]
+        public void GetCountryNames_OnInvoke_ReturnsSuccessWithCountryNames()
+        {
+            var result = _controller.GetNames().Result as OkObjectResult;
+
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
+            result.Value.As<IEnumerable<CountryModel>>().Should().NotBeEmpty();
+        }
+    }
+}
---
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Controllers/CountryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Paymentsense.Coding.Challenge.Api.Services;
+
+namespace Paymentsense.Coding.Challenge.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+
+        public CountryController(
+            ICountryService countryService)
+        {
+            _countryService = countryService ?? throw new ArgumentNullException(nameof(countryService));
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Paymentsense Coding Challenge!");
+        }
+
+        [HttpGet]
+        [Route("/names")]
+        public ActionResult<string[]> GetNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
