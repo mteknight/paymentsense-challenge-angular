29aeef6bc9082ab87f8bb645b0cbef69804e1ca7
Alexandre Fernandes <mteknight@gmail.com>
2020-07-12 21:19:27 +0100

    refactored the code for cleanness and quality

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Services/CountryService.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Services/CountryService.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Paymentsense.Coding.Challenge.Api.Models;
+
 namespace Paymentsense.Coding.Challenge.Api.Services
 {
     public class CountryService : ICountryService
     {
+        private const string Api = "https://restcountries.eu/rest/v2";
         private readonly IHttpClientFactory _httpClientFactory;
 
         public CountryService(
@@ -21,29 +25,48 @@ namespace Paymentsense.Coding.Challenge.Api.Services
         public async Task<IEnumerable<string>> GetNames(
             CancellationToken cancellationToken)
         {
+            var countries = await GetCountries(cancellationToken)
+                .ConfigureAwait(false);
+
+            return countries
+                .Select(country => country.Name);
+        }
+
+        private async Task<IEnumerable<CountryModel>> GetCountries(CancellationToken cancellationToken)
+        {
+            var uri = new Uri($"{Api}/all");
+            var contentStream = await GetFromApi(uri, cancellationToken)
+                .ConfigureAwait(false);
+
+            return await Deserialize<IEnumerable<CountryModel>>(contentStream, cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        private async Task<Stream> GetFromApi(
+            Uri uri,
+            CancellationToken cancellationToken)
+        {
             using var client = _httpClientFactory.CreateClient();
 
-            var response = await client.GetAsync("https://restcountries.eu/rest/v2/all", cancellationToken)
+            var response = await client.GetAsync(uri, cancellationToken)
                 .ConfigureAwait(false);
 
-            var contentStream = await response.Content.ReadAsStreamAsync()
-                .ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
 
+            return await response.Content.ReadAsStreamAsync()
+                .ConfigureAwait(false);
+        }
+
+        private static ValueTask<TOut> Deserialize<TOut>(
+            Stream stream,
+            CancellationToken cancellationToken)
+        {
             var options = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             };
 
-            var countries = await JsonSerializer.DeserializeAsync<IEnumerable<CountryModel>>(contentStream, options, cancellationToken)
-                .ConfigureAwait(false);
-
-            return countries
-                .Select(country => country.Name as string);
+            return JsonSerializer.DeserializeAsync<TOut>(stream, options, cancellationToken);
         }
-    }
-
-    public class CountryModel
-    {
-        public string Name { get; set; }
     }
 }
